4a1e42f152d0ba69f39c373265820ee1accbeb16
HugoHugoHugoHugoHugo <105725295+HugoHugoHugoHugoHugo@users.noreply.github.com>
2025-12-04 17:31:17 +0100

    préparation test overlap

--- FileManager.js
+++ FileManager.js
@@ -22,64 +22,64 @@ class FileManager{
         let filePath;
         switch(nomCours[0]){
             case "A" :
-                filePath="./SujetA_data/AB/edt.cru";
+                filePath=FileManager.root+"/SujetA_data/AB/edt.cru";
                 break;
             case "B" :
-                filePath="./SujetA_data/AB/edt.cru";
+                filePath=FileManager.root+"/SujetA_data/AB/edt.cru";
                 break;
             case "C" :
-                filePath="./SujetA_data/CD/edt.cru";
+                filePath=FileManager.root+"/SujetA_data/CD/edt.cru";
                 break;
             case "D" :
-                filePath="./SujetA_data/CD/edt.cru";
+                filePath=FileManager.root+"/SujetA_data/CD/edt.cru";
                 break;
             case "E" :
-                filePath="./SujetA_data/EF/edt.cru";
+                filePath=FileManager.root+"/SujetA_data/EF/edt.cru";
                 break;
             case "F" :
-                filePath="./SujetA_data/EF/edt.cru";
+                filePath=FileManager.root+"/SujetA_data/EF/edt.cru";
                 break;
             case "G" :
-                filePath="./SujetA_data/GH/edt.cru";
+                filePath=FileManager.root+"/SujetA_data/GH/edt.cru";
                 break;
             case "H" :
-                filePath="./SujetA_data/GH/edt.cru";
+                filePath=FileManager.root+"/SujetA_data/GH/edt.cru";
                 break;
             case "I" :
-                filePath="./SujetA_data/IJ/edt.cru";
+                filePath=FileManager.root+"/SujetA_data/IJ/edt.cru";
                 break;
             case "J" :
-                filePath="./SujetA_data/IJ/edt.cru";
+                filePath=FileManager.root+"/SujetA_data/IJ/edt.cru";
                 break;
             case "K" :
-                filePath="./SujetA_data/KL/edt.cru";
+                filePath=FileManager.root+"/SujetA_data/KL/edt.cru";
                 break;
             case "L" :
-                filePath="./SujetA_data/KL/edt.cru";
+                filePath=FileManager.root+"/SujetA_data/KL/edt.cru";
                 break;
             case "M" :
-                filePath="./SujetA_data/MN/edt.cru";
+                filePath=FileManager.root+"/SujetA_data/MN/edt.cru";
                 break;
             case "N" :
-                filePath="./SujetA_data/MN/edt.cru";
+                filePath=FileManager.root+"/SujetA_data/MN/edt.cru";
                 break;
             case "O" :
-                filePath="./SujetA_data/OP/edt.cru";
+                filePath=FileManager.root+"/SujetA_data/OP/edt.cru";
                 break;
             case "P" :
-                filePath="./SujetA_data/OP/edt.cru";
+                filePath=FileManager.root+"/SujetA_data/OP/edt.cru";
                 break;
             case "Q" :
-                filePath="./SujetA_data/QR/edt.cru";
+                filePath=FileManager.root+"/SujetA_data/QR/edt.cru";
                 break;
             case "R" :
-                filePath="./SujetA_data/QR/edt.cru";
+                filePath=FileManager.root+"/SujetA_data/QR/edt.cru";
                 break;
             case "S" :
-                filePath="./SujetA_data/ST/edt.cru";
+                filePath=FileManager.root+"/SujetA_data/ST/edt.cru";
                 break;
             case "T" :
-                filePath="./SujetA_data/ST/edt.cru";
+                filePath=FileManager.root+"/SujetA_data/ST/edt.cru";
                 break;
             default:
                 return -1;
@@ -118,7 +118,7 @@ class FileManager{
     static next(){
         const returnValue = this.folders[this.index];
         this.index++;
-        return ("./SujetA_data/"+returnValue+"/edt.cru");
+        return (FileManager.root+"/SujetA_data/"+returnValue+"/edt.cru");
     }
 
     /**
